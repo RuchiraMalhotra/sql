4e4dcf52f136277296792b8fea0e401859446246
Ruchira Malhotra <ruchira.toronto@gmail.com>
2025-08-15 14:29:46 -0400

    Update submission

--- 02_activities/assignments/assignment2.sql
+++ 02_activities/assignments/assignment2.sql
@@ -1,5 +1,8 @@
 /* ASSIGNMENT 2 */
+
 /* SECTION 2 */
+
+
 
 -- COALESCE
 /* 1. Our favourite manager wants a detailed long list of products, but is afraid of tables! 
@@ -20,7 +23,18 @@ The `||` values concatenate the columns into strings.
 Edit the appropriate columns -- you're making two edits -- and the NULL rows will be fixed. 
 All the other rows will remain the same.) */
 
+SELECT 
+product_name || ', ' || product_size || ' (' || product_qty_type || ')'
+FROM product;
 
+SELECT *
+FROM product
+WHERE product_size IS NULL
+OR product_qty_type IS NULL;
+
+SELECT 
+COALESCE(product_size, ' ') || ', ' || product_name || ' (' || COALESCE(product_qty_type, 'unit') || ')'
+FROM product;
 
 --Windowed Functions
 /* 1. Write a query that selects from the customer_purchases table and numbers each customer’s  
@@ -32,17 +46,61 @@ each new market date for each customer, or select only the unique market dates p
 (without purchase details) and number those visits. 
 HINT: One of these approaches uses ROW_NUMBER() and one uses DENSE_RANK(). */
 
+SELECT 
+ customer_id,
+ market_date,
+DENSE_RANK() OVER (
+        PARTITION BY customer_id
+        ORDER BY market_date
+    ) AS visit_number
+FROM (
+    SELECT DISTINCT customer_id, market_date
+    FROM customer_purchases
+) AS visits;
 
 
 /* 2. Reverse the numbering of the query from a part so each customer’s most recent visit is labeled 1, 
 then write another query that uses this one as a subquery (or temp table) and filters the results to 
 only the customer’s most recent visit. */
 
+SELECT
+    customer_id,
+    market_date,
+    DENSE_RANK() OVER (
+        PARTITION BY customer_id
+        ORDER BY market_date DESC
+    ) AS visit_number
+FROM (
+    SELECT DISTINCT customer_id, market_date
+    FROM customer_purchases
+) AS visits;
 
+SELECT *
+FROM (
+    SELECT
+        customer_id,
+        market_date,
+        DENSE_RANK() OVER (
+            PARTITION BY customer_id
+            ORDER BY market_date DESC
+        ) AS visit_number
+    FROM (
+        SELECT DISTINCT customer_id, market_date
+        FROM customer_purchases
+    ) AS visits
+) AS ranked_visits
+WHERE visit_number = 1;
 
 /* 3. Using a COUNT() window function, include a value along with each row of the 
 customer_purchases table that indicates how many different times that customer has purchased that product_id. */
-
+SELECT
+    customer_id,
+    product_id,
+    market_date,
+    COUNT(*) OVER (
+        PARTITION BY customer_id, product_id
+    ) AS product_purchase_count
+FROM customer_purchases;
 
 
 -- String manipulations
@@ -56,11 +114,23 @@ Remove any trailing or leading whitespaces. Don't just use a case statement for 
 | Habanero Peppers - Organic | Organic     |
 
 Hint: you might need to use INSTR(product_name,'-') to find the hyphens. INSTR will help split the column. */
+SELECT
+    product_name,
+    TRIM(
+        SUBSTR(
+            product_name,
+            INSTR(product_name, '-') + 1
+        )
+    ) AS description
+FROM product;
 
 
 
 /* 2. Filter the query to show any product_size value that contain a number with REGEXP. */
 
+SELECT *
+FROM product
+WHERE product_size REGEXP '[0-9]';
 
 
 -- UNION
@@ -73,13 +143,31 @@ HINT: There are a possibly a few ways to do this query, but if you're struggling
 3) Query the second temp table twice, once for the best day, once for the worst day, 
 with a UNION binding them. */
 
+WITH daily_sales AS (
+    SELECT
+        market_date,
+        SUM(quantity * cost_to_customer_per_qty) AS total_sales
+    FROM customer_purchases
+    GROUP BY market_date
+)
+SELECT *
+FROM daily_sales;
 
 
+WITH ranked_sales AS (
+    SELECT
+        market_date,
+        total_sales,
+        RANK() OVER (ORDER BY total_sales DESC) AS best_rank,
+        RANK() OVER (ORDER BY total_sales ASC)  AS worst_rank
+    
+)
+FROM daily_sales
 
 /* SECTION 3 */
 
--- Cross Join
-/*1. Suppose every vendor in the `vendor_inventory` table had 5 of each of their products to sell to **every** 
+-- Cross Join 
+/*1. Suppose every vendor, in the `vendor_inventory` table had 5 of each of their products to sell to **every** 
 customer on record. How much money would each vendor make per product? 
 Show this by vendor_name and product name, rather than using the IDs.
 
